305c81b69d35571f20e75823243460c0247ad743
Jess Hause Ugray <jess.ugray@gmail.com>
2021-12-16 11:05:26 -0500

    thought routes

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,9 +1,32 @@
 const router = require('express').Router();
+const {
+  getAllThought,
+  getThoughtById,
+  createThought,
+  updateThought,
+  deleteThought,
+  createReaction,
+  deleteReaction
+} = require('../../controllers/comment-controller');
 
+// /api/thoughts
+router
+  .route('/')
+  .get(getAllThought)
+  .post(createThought)
 
+// /api/thoughts/:id
+router
+  .route('/:id')
+  .get(getThoughtById)
+  .put(updateThought)
+  .delete(deleteThought);
 
-
-
+// /api/thoughts/:thoughtID/reactions
+router  
+  .route('/:thoughtID/reactions')
+  .post(createReaction)
+  .delete(deleteReaction);
 
 
 module.exports = router;
