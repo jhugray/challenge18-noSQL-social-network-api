f246611b99957f205718bf6b06221b5ff3c1b06b
Jess Hause Ugray <jess.ugray@gmail.com>
2021-12-16 05:39:30 -0500

    user model

--- models/User.js
+++ models/User.js
@@ -1,22 +1,46 @@
 const { Schema, model } = require('mongoose');
-const dateFormat = require('../utils/dateFormat');
+// const dateFormat = require('../utils/dateFormat');
 
 const UserSchema = new Schema(
   {
     username: {
-
+      type: String,
+      unique: true,
+      required: true,
+      trim: true
     },
     email: {
-
+      type: String,
+      unique: true,
+      required: true,
+      match: /.+\@.+\..+/
     },
-    thoughts: {
+    thoughts: [
+      {
+       type: Schema.Types.ObjectId,   //check this
+       ref: 'Thought' 
+      }
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId, //check this
+        ref: 'User' 
+      }
 
+    ]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
     },
-    friends: {
-
-    },
-
-
-
+    id: false
   }
 )
+
+UserSchema.virtual('friendCount').get(function() { // check this
+  return this.friends.length;
+})
+
+const User = model('User', UserSchema);
+module.exports = User;
